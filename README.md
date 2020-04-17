47a892f508a5fe76c6fc9410db94a55204188de2
EliseoNunezVega <eliseonunezvega@gmail.com>
2020-04-17 16:44:22 -0700

    -m "added comments and Readme text"

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# Object-Detection
-Just put the test flight files somewhere in here and I'll organize it later - Amanda
+# Shape & Letter Detection
+
+How it works:
+-------------
+<i>The main function is detect.py; the entire process will begin here</i>
+ 
+<b>Step 1: Shape Region Estimation & Cropping </b>
+
+ <div>
+   <p>From detect.py, we call our shape_detector function. This function will be responsible for returning a masked image containing only the regions it believes to contain any shapes</p> 
+ </div>
+
--- shape_detection.py
+++ shape_detection.py
@@ -3,25 +3,28 @@
 import cv2
 import numpy as np
 
-
 def shape_detector(gray_image):
 
-    # sharpening image
+    # sharpening image; make edges stand out
     pre_bilateral = cv2.bilateralFilter(gray_image, 7, 140, 140)
     sharpened = cv2.addWeighted(gray_image, 1, gray_image - pre_bilateral, 3, 0)
 
-    # smoothing images (to remove natural,random textures and keep only straight edges)
-    median = cv2.medianBlur(sharpened, 9)
+    # smoothing images (to remove natural,random textures --ex. grass --- and keep only straight edges)
+    median = cv2.medianBlur(sharpened, 7)
 
-    # sharpening median (emphasizing features to make median filter more effective)
+    # sharpening noise (emphasizing noise features to make median filter more effective at eliminating them)
     pre_bilateral2 = cv2.bilateralFilter(median, 7, 140, 140)
     sharpened2 = cv2.addWeighted(median, 1, median - pre_bilateral2, 3, 0)
 
-    # smooth median again (minimizes background noise)
-    median2 = cv2.medianBlur(sharpened2, 9)
+    # smooth image again (minimizes background noise)
+    median2 = cv2.medianBlur(sharpened2, 7)
+
+    # final sharpening before canny edge detection (makes for better edges)
+    pre_bilateral3 = cv2.bilateralFilter(median2, 7, 140, 140)
+    sharpened3 = cv2.addWeighted(median2, 1, median2 - pre_bilateral3, 3, 0)
 
     # canny edge detection
-    canny = cv2.Canny(median2, 0, 80, 1)
+    canny = cv2.Canny(sharpened3, 0, 80, 1)
 
     while 1:
         k = cv2.waitKey(1) & 0xFF
@@ -37,6 +40,8 @@ def shape_detector(gray_image):
         cv2.imshow('prebilat2', pre_bilateral2)
         cv2.imshow('sharpened2', sharpened2)
         cv2.imshow('median2', median2)
+        cv2.imshow('prebilat3', pre_bilateral3)
+        cv2.imshow('sharpened3', sharpened3)
         cv2.imshow('canny', canny)
 
         if k == ord('q'):
