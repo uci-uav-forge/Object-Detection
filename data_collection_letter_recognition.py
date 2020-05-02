2f6d56532d4e5fb07c7fc407ce126619cd7c38f5
EliseoNunezVega <eliseonunezvega@gmail.com>
2020-05-01 20:23:33 -0700

    now using HOG features instead of pixel value features for better accuracy

--- data_collection_letter_recognition.py
+++ data_collection_letter_recognition.py
@@ -2,84 +2,108 @@ import cv2
 import numpy as np
 import glob
 import os
+from HOG_feature_extraction import hog_extractor
 
-################################################################################
-# variables
-################################################################################
-# number of data points used to train KNN model
-NUMBER_OF_DATA_POINTS = 250
+############################
+# Declaring Useful Variables
+############################
+# number of data points used to train for numbers
+NUMBER_OF_DATA_POINTS_NUM = 200
 
-# variable used to keep track of data points
-number_of_data_points_counter = NUMBER_OF_DATA_POINTS
+# number of data points used to train for alphabet
+NUMBER_OF_DATA_POINTS_ALPHA = 200
+
+# variable used to keep track of number data points
+number_data_points_counter = NUMBER_OF_DATA_POINTS_NUM
+
+# variable used to keep track of alphabet data points
+alpha_data_points_counter = NUMBER_OF_DATA_POINTS_ALPHA
 
 # creating labels for alphabet
-alphabet_complete = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                     'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+alphabet_label_array = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
-alphabet_complete_ascii = [ord(x) for x in alphabet_complete]
+alphabet_label_array_ascii = [ord(x) for x in alphabet_label_array]
 
-alphabet_complete_ascii = np.array(alphabet_complete_ascii)
-alphabet_complete_labels = np.repeat(alphabet_complete_ascii, NUMBER_OF_DATA_POINTS)
+alphabet_label_array_ascii = np.array(alphabet_label_array_ascii)
+alphabet_label_array_ascii = np.repeat(alphabet_label_array_ascii, NUMBER_OF_DATA_POINTS_ALPHA)
+alphabet_label_array_ascii = np.array(alphabet_label_array_ascii, dtype=np.float32)
 
 # creating labels for numbers
 nums = np.arange(10)
-num_labels = np.repeat(nums, NUMBER_OF_DATA_POINTS)
+number_training_labels = np.repeat(nums, NUMBER_OF_DATA_POINTS_NUM)
+number_training_labels = np.array(number_training_labels, dtype=np.float32)
 
-# this is our complete training array, containing numbers and alphabet images
-training_images = []
+# this is our number training array
+number_training_images = []
+
+# this is our alphabet training array
+alphabet_training_images = []
 
 # this is where we will keep all of our labels
-training_labels = np.concatenate((num_labels, alphabet_complete_labels), axis=None)
-training_labels = np.array(training_labels, dtype=np.float32)
+# training_labels = np.concatenate((num_labels, alphabet_complete_labels), axis=None)
+# training_labels = np.array(training_labels, dtype=np.float32)
 
 #################################################################################
-# loading number images into training_images array
+# Loading Number Images
 #################################################################################
 for indx, num in enumerate(nums):
     curr_folder = str(indx)
     path_argument = 'TrainingImages\\' + curr_folder + '\\*.png'
     for filename in glob.glob(path_argument):
         # ensuring that we do not exceed the set data point amount
-        if number_of_data_points_counter > 0:
+        if number_data_points_counter > 0:
             # converting to grayscale
             curr_image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-            # flattening the image to 1 dimension
-            curr_image = curr_image.flatten()
+            # extracting hog features and flattening
+            curr_image = hog_extractor(curr_image)
             # appending this one dimensional array to the end of another array
-            training_images.append(curr_image)
+            number_training_images.append(curr_image)
             # reducing the counter
-            number_of_data_points_counter = number_of_data_points_counter - 1
+            number_data_points_counter = number_data_points_counter - 1
         else:
             # resetting data point counter
-            number_of_data_points_counter = NUMBER_OF_DATA_POINTS
+            number_data_points_counter = NUMBER_OF_DATA_POINTS_NUM
             break
 ##############################################################################
-# loading alphabet images to lower_case and upper_case arrays
+# Loading Alphabet Images
 ##############################################################################
-for indx, letter in enumerate(alphabet_complete):
+for indx, letter in enumerate(alphabet_label_array):
     curr_folder = letter
     path_argument = 'TrainingImages\\' + curr_folder + '\\*.png'
     for filename in glob.glob(path_argument):
         # ensuring that we do not exceed the set data point amount
-        if number_of_data_points_counter > 0:
+        if alpha_data_points_counter > 0:
             # converting to grayscale
             curr_image = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
-            # flattening the image to 1 dimension
-            curr_image = curr_image.flatten()
+            # extracting hog features and flattening
+            curr_image = hog_extractor(curr_image)
             # appending this one dimensional array to the end of another array
-            training_images.append(curr_image)
+            alphabet_training_images.append(curr_image)
             # reducing the counter
-            number_of_data_points_counter = number_of_data_points_counter - 1
+            alpha_data_points_counter = alpha_data_points_counter - 1
         else:
             # resetting data point counter
-            number_of_data_points_counter = NUMBER_OF_DATA_POINTS
+            alpha_data_points_counter = NUMBER_OF_DATA_POINTS_ALPHA
             break
 
-# converting our array into numpy array for training
-training_images = np.array(training_images, dtype=np.float32)
+###########################
+# Saving Images and Labels
+###########################
 
-# saving our training data
-np.savez('knn_data.npz', training_images=training_images, training_labels=training_labels)
+# converting our image training images array into numpy array
+alphabet_training_images = np.array(alphabet_training_images, dtype=np.float32)
+
+# converting our number training images array into numpy array
+number_training_images = np.array(number_training_images, dtype=np.float32)
+
+# saving our number training data
+np.savez('knn_number_data.npz', number_training_images=number_training_images,
+         number_training_labels=number_training_labels)
+
+# saving our alphabet training data
+np.savez('knn_alphabet_data.npz', alphabet_training_images=alphabet_training_images,
+         alphabet_training_labels=alphabet_label_array_ascii)
 
 
 
--- letter_recognition.py
+++ letter_recognition.py
@@ -1,38 +1,72 @@
 import cv2
 import numpy as np
+from HOG_feature_extraction import hog_extractor
 
 # constant image dimensions (test images must match these dimensions)
 HEIGHT = 128
 WIDTH = 128
 
-# Loading training data
-with np.load('knn_data.npz') as data:
-    print(data.files)
-    training_data = data['training_images']
-    training_labels = data['training_labels']
+# loading number training data
+with np.load('knn_number_data.npz') as data:
+    number_training_data = data['number_training_images']
+    number_training_labels = data['number_training_labels']
+
+# loading alphabet training data
+with np.load('knn_alphabet_data.npz') as data:
+    alphabet_training_data = data['alphabet_training_images']
+    alphabet_training_labels = data['alphabet_training_labels']
+
+# reading test image
+test_image_arr = []
+test_image = cv2.imread('TestImage\\6.png', cv2.IMREAD_GRAYSCALE)
+
+# extracting hog features and flattening
+test_image = hog_extractor(test_image)
+
+test_image_arr.append(test_image)
+test_image = np.array(test_image_arr, dtype=np.float32)
 
 # creating an instance of the KNN class
 knn = cv2.ml.KNearest_create()
 
-# training our model with our images and labels
-knn.train(training_data, cv2.ml.ROW_SAMPLE, training_labels)
+########################################
+# Training and Testing Numbers
+#########################################
+# training our model with our number images
+knn.train(number_training_data, cv2.ml.ROW_SAMPLE, number_training_labels)
 
-# reading test image
-test_image_arr = []
-test_image = cv2.imread('TestImage\\2.png', cv2.IMREAD_GRAYSCALE)
-if test_image.shape[0] != 128 or test_image.shape[1] != 128:
-    test_image = cv2.resize(test_image, (WIDTH, HEIGHT))
-test_image = test_image.flatten()
-test_image_arr.append(test_image)
-test_image = np.array(test_image_arr, dtype=np.float32)
+# testing out our model for numbers
+ret_num, result_num, neighbours_num, dist_num = knn.findNearest(test_image, k=3)
 
-# testing out our model
-ret, result, neighbours, dist = knn.findNearest(test_image, k=3)
+#########################################
+# Training and Testing Alphabet
+#########################################
+# training our model with alphabet images
+knn.train(alphabet_training_data, cv2.ml.ROW_SAMPLE, alphabet_training_labels)
+
+# testing out our model for alphabet
+ret_alpha, result_alpha, neighbours_alpha, dist_alpha = knn.findNearest(test_image, k=3)
+
+##########################################
+# Taking Best Estimation Based on Distance
+##########################################
+
+average_num_distance = np.mean(dist_num/np.sum(dist_alpha))
+average_alpha_distance = np.mean(dist_alpha/np.sum(dist_alpha))
+
+if average_alpha_distance < average_num_distance:
+    result = int(ret_alpha)
+
+if average_num_distance < average_alpha_distance:
+    result = int(ret_num)
 
 # if the result is not an integer give us the ascii character
-
 if result > 10:
-    result = chr(int(result[0][0]))
+    result = chr(result)
 
+print(chr(int(ret_alpha)))
+print(ret_num)
+print("Average distance for num:", average_num_distance)
+print("Average distance for alpha:", average_alpha_distance)
 # printing result
-print(result)
+print("The computer thinks this is", result)
