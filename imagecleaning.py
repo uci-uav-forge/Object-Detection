3f998c3663a3acbb504a47fb265a34f37cb569d9
Harry918 <49503653+Harry918@users.noreply.github.com>
2020-05-15 18:54:41 -0700

    Update imagecleaning.py

--- imagecleaning.py
+++ imagecleaning.py
@@ -4,10 +4,54 @@ import pdb
 #669*497
 
 def main():
-	img = cv.imread('test.jpg')
-	kernel = np.ones((5,5),np.uint8)
+	red = []
+	green = [];
+	blue = [];
+	img = cv.imread('brown.jpg', cv.IMREAD_GRAYSCALE)
+	for i in range(img.shape[0]):
+		for j in range(img.shape[1]):
+			if(img[i][j] > 0):
+				red.append([i, j])
+	print(len(red))
+	if(len(red) > 10000):
+		for i in red:
+			img[i[0]][i[1]] = 0;
+	kernel = np.ones((3,5),np.uint8)
 	img = cv.morphologyEx(img, cv.MORPH_OPEN, kernel)
+	# print(img.shape[1])
+	# for i in range(img.shape[0]):
+	# 	for j in range(img.shape[1]):
+	# 		if(img[i][j][0] > img[i][j][1]):
+	# 			if(img[i][j][0] > img[i][j][2]):
+	# 				red.append([i, j])
+	# 			else:
+	# 				blue.append([i, j])
+	# 		elif(img[i][j][0] < img[i][j][1]):
+	# 			if(img[i][j][1] > img[i][j][2]):
+	# 				green.append([i, j])
+	# 			else:
+	# 				blue.append([i, j])
+	# for i in red:
+	# 	img[i[0]][i[1]][0] = 0
+	# 	img[i[0]][i[1]][1] = 0
+	# 	img[i[0]][i[1]][2] = 0
+	#
+	# # for i in green:
+	# # 	img[i[0]][i[1]][0] = 0
+	# # 	img[i[0]][i[1]][1] = 0
+	# # 	img[i[0]][i[1]][2] = 0
+	#
+	# for i in blue:
+	# 	img[i[0]][i[1]][0] = 0
+	# 	img[i[0]][i[1]][1] = 0
+	# 	img[i[0]][i[1]][2] = 0
+	cv.imshow('image',img)
+	cv.waitKey(0)
+	cv.destroyAllWindows()
 
+
+	# img = cv.dilate(img,kernel,iterations = 1)
+	# cv.threshold(img,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
 	# for i in range(len(img) - 2):
 	# 	for j in range(len(img[1]) - 2):
 	# 		if((img[i][j][1] != img[i+1][j][1] or img[i][j][1] != img[i][j+1][1]) and img[i][j][1] > 0):
@@ -29,9 +73,6 @@ def main():
 	# print(maxi, maxj)
 	# for i in range(15):
 	# 	img[i+maxi][maxj][0] = 255
-	cv.imshow('image',img)
-	cv.waitKey(0)
-	cv.destroyAllWindows()
 
 # def checkBlobs(img, i, j, equalColorR, equalColorG, equalColorB, tempBlobSize):
 # 	if(i<0 or j<0 or i>len((img) - 1) or j>len((img[1]) - 1)):
