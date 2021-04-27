3e9bc878b9aa4d0132b6996bbd1618c1a5616997
dangthanhan507 <45112823+dangthanhan507@users.noreply.github.com>
2021-04-27 10:15:41 -0700

    Update main.py
    added comments

--- src/main.py
+++ src/main.py
@@ -25,29 +25,35 @@ def main():
     img = cv2.imread(img_path)
 
     #detected color image. if returns None (not found) then process is over.
-
     pack = detect_color(img) #this takes awhile to run
     if type(pack) == type(None):
         print('No interesting color detected.')
         return 0
 
+    #unpack both images for shape and letter detection.
     color_img, orig_img = pack
 
     #NOTE: try to find a way to take out the gray spots in an image. I see it a lot, and if we do that, then most of the problems disappear.
+    
+    #fill in is to get rid of letter inside detected shape, so it won't mess with contour detection.
     shape_img = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
     shape_img = fill_in(shape_img)
 
-
+    
+    #shape_detection / center coordinate stuff
     cv2.imwrite('../filled_shape/shape.jpg', shape_img) #this is to get rid of error in shape_detect could be done better. will update when i have time.
     coord, bound, shape, detected_shape = detectShape('../filled_shape/shape.jpg')
 
+    
     x_bound, y_bound, w, h = bound
+    
+    #uses bounding box to get only the detected shape in order to do letter_recognition
     crop_color = orig_img[y_bound-5: y_bound+h+5, x_bound-5:x_bound+w+5]
     crop_gray = cv2.cvtColor(crop_color,cv2.COLOR_BGR2GRAY)
 
 
     crop = crop_shape(crop_gray)
-    cv2.imwrite('filled_shape/test.jpg', crop)
+    #cv2.imwrite('filled_shape/test.jpg', crop) 
 
     character = letter_recog(crop,7,7)
 
